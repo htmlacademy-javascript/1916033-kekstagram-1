8366bdc00bca087739cda0e4b6e223fa64e75852
h1ndy <155265302+h1ndy@users.noreply.github.com>
2024-06-14 18:15:29 +0300

    Открывается и закрывается

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-const NAMES = [
+import {getRandomInteger, getRandomArrayElement} from './util.js';
+const Names = [
   'Иван',
   'Валера',
   'Мария',
@@ -8,7 +9,7 @@ const NAMES = [
   'Лола',
   'Джерси',
 ];
-const COMMENTS = [
+const Comments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -16,13 +17,17 @@ const COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const LIKES = {
+const Likes = {
   MIN: 15,
   MAX: 200
 };
-const QUANTITY_COMMENTS = {
+const QuantityComments = {
   MIN: 0,
   MAX: 30
+};
+const RandomAvatars = {
+  MIN: 1,
+  MAX: 6
 };
 const SIMILAR_PHOTOS_COUNT = 25;
 const createCount = () => {
@@ -31,35 +36,22 @@ const createCount = () => {
     return ++count;
   };
 };
-const RANDOM_AVATARS = {
-  MIN: 0,
-  MAX: 6
-};
-const getIdComment = createCount();
-const getIdPhoto = createCount();
-const getUnicUrl = createCount();
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const idComment = createCount();
+const idPhoto = createCount();
+const unicUrl = createCount();
 const createComment = () => ({
-  id: getIdComment(),
-  avatar: `img/avatar-${getRandomInteger(RANDOM_AVATARS.MIN, RANDOM_AVATARS.MAX)}.svg`,
-  message: getRandomArrayElement(COMMENTS),
-  name: getRandomArrayElement(NAMES),
+  id: idComment(),
+  avatar: `img/avatar-${getRandomInteger(RandomAvatars.MIN, RandomAvatars.MAX)}.svg`,
+  message: getRandomArrayElement(Comments),
+  name: getRandomArrayElement(Names),
 });
-function createUserPhoto() {
-  return ({
-    id: getIdPhoto(),
-    url: `photos/${getUnicUrl()}.jpg`,
-    description: 'Сфотографировал недавно на выходных',
-    likes: getRandomInteger(LIKES.MIN, LIKES.MAX),
-    comments: Array.from({ length: getRandomInteger(QUANTITY_COMMENTS.MIN, QUANTITY_COMMENTS.MAX) }, createComment)
-  });
-}
+const createUserPhoto = () => ({
+  id: idPhoto(),
+  url: `photos/${unicUrl()}.jpg`,
+  description: 'Классно получилось?',
+  likes: getRandomInteger(Likes.MIN, Likes.MAX),
+  comments:
+  Array.from({length: getRandomInteger(QuantityComments.MIN, QuantityComments.MAX)}, createComment)
+});
 const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createUserPhoto);
-similarPhotos.values();
 export {similarPhotos};
